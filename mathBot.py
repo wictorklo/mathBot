0b708296ba851f11b2bb8cc0cbc0e62a596fcebc
Wictor Jensen <wict0010@Wictors-MacBook-Pro.local>
2016-03-30 14:48:07 +0200

    Can read and write to file. Still tries to post duplicate tweets

--- mathBot.py
+++ mathBot.py
@@ -1,9 +1,16 @@
 import tweepy
 import time
+import os
 import sys
 import re
 import math
 from secrets import *
+
+if not os.path.exists('repliedTweets.txt'):
+    open('repliedTweets.txt', 'w').close()
+
+with open('repliedTweets.txt', 'rU') as in_file:
+	calculatedTweets = str(in_file.read()).split('\n')
 
 auth = tweepy.OAuthHandler(C_KEY, C_SECRET)  
 auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)  
@@ -15,29 +22,30 @@ calculatedTweets = []
 def calculate(tweet):
 	match = re.search(r'([-+]?[0-9]*\.?[0-9]+\s*[\/\+\-\*\^]\s*)+([-+]?[0-9]*\.?[0-9]+)', tweet.text)
 
-	if match and tweet.id not in calculatedTweets:
+	if match and str(tweet.id) not in calculatedTweets:
 		print(match.group())
 		expression = str(match.group())
 		print(expression + " = " + str(eval(expression)))
 		api.update_status(expression + " = " + str(eval(expression)) + " @" + str(tweet.user.screen_name), tweet.id)
-		calculatedTweets.append(tweet.id)
+		calculatedTweets.append(str(tweet.id))
 
 
-	elif tweet.id not in calculatedTweets:
+	elif str(tweet.id) not in calculatedTweets:
 		print("Could not find any matches")
 		api.update_status("Could not find math expression @" + str(tweet.user.screen_name), tweet.id )
-		calculatedTweets.append(tweet.id)
+		calculatedTweets.append(str(tweet.id))
 
 
 for status in mentions:
-	if status.id not in calculatedTweets:
+	if str(status.id) not in calculatedTweets:
 		print(str(status.user.screen_name) + " : " + status.text)
 		print(status.id)
 		calculate(status)
 		print("\n\n")
 		#api.update_status("Test reply @" + str(status.user.screen_name), status.id)
 
-
+with open('repliedTweets.txt', 'w', ) as out_file:
+	out_file.write('\n'.join(calculatedTweets))
 
 #api.update_status("Testing tweet 1", )
 time.sleep(3)
