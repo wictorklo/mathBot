1f5ef112bd9d5bfaa14be4c49a83280bbf7a443a
Wictor Jensen <wict0010@Wictors-MacBook-Pro.local>
2016-03-30 14:17:25 +0200

    Added reply and made it only able to reply to a tweet once per run

--- mathBot.py
+++ mathBot.py
@@ -12,28 +12,32 @@ api = tweepy.API(auth)
 mentions = api.mentions_timeline()
 calculatedTweets = []
 
-def calculate(text):
-	match = re.search(r'([-+]?[0-9]*\.?[0-9]+\s*[\/\+\-\*\^]\s*)+([-+]?[0-9]*\.?[0-9]+)', tweet)
+def calculate(tweet):
+	match = re.search(r'([-+]?[0-9]*\.?[0-9]+\s*[\/\+\-\*\^]\s*)+([-+]?[0-9]*\.?[0-9]+)', tweet.text)
 
-	if match:
+	if match and tweet.id not in calculatedTweets:
 		print(match.group())
 		expression = str(match.group())
 		print(expression + " = " + str(eval(expression)))
+		api.update_status(expression + " = " + str(eval(expression)) + " @" + str(tweet.user.screen_name), tweet.id)
+		calculatedTweets.append(tweet.id)
 
 
-	else:
+	elif tweet.id not in calculatedTweets:
 		print("Could not find any matches")
+		api.update_status("Could not find math expression @" + str(tweet.user.screen_name), tweet.id )
+		calculatedTweets.append(tweet.id)
 
 
 for status in mentions:
-	print(str(status.user.screen_name) + " : " + status.text)
-	print(status.id)
-	tweet = status.text
-	calculate(tweet)
-	print("\n\n")
-	calculatedTweets.append(status.id)
+	if status.id not in calculatedTweets:
+		print(str(status.user.screen_name) + " : " + status.text)
+		print(status.id)
+		calculate(status)
+		print("\n\n")
+		#api.update_status("Test reply @" + str(status.user.screen_name), status.id)
 
 
 
-#api.update_status("Testing tweet 1")
+#api.update_status("Testing tweet 1", )
 time.sleep(3)
